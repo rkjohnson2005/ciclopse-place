86a5fcf6376d92b97af352b8b00cffa101bbb07c
Richard Johnson <development@rkjohn.com>
2018-10-15 13:00:25 -0400

    Added constructor

--- src/CICLOPSE/Place/Place.php
+++ src/CICLOPSE/Place/Place.php
@@ -4,7 +4,8 @@ use CICLOPSE;
 
 class Place extends CICLOPSE\Standard
 {
-    public static function hello() {
-        return 'hello';
+    public function __construct($table_id = '')
+    {
+        parent::__construct('place', 'Rickzilla', $table_id);
     }
 }
